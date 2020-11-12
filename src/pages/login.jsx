4d3b5162ba3c51aaf35b8342d5d732ea395ea693
weslynSouza <wesley.i476@gmail.com>
2020-11-12 16:09:10 -0300

    Criando o projeto junto com a pagina de login

---
+++ src/pages/login.jsx
@@ -0,0 +1,52 @@
+import { SiFacebook, SiApple} from 'react-icons/si'
+import { FcGoogle } from 'react-icons/fc';
+import { FiArrowRight } from 'react-icons/fi';
+import { FaCog } from 'react-icons/fa';
+
+import logo from '../assets/lol-logo.png';
+import wallpaper from '../assets/tk-chef-splash.jpg';
+
+import '../styles/login.css';
+
+function Login(){
+    return (
+        <div className="container">
+            <div className="container-form">
+                <div className="logo">
+                    <img src={logo} alt="logo"/>
+                </div>
+
+                <h1>Fazer login</h1>
+
+                <form>
+                    <input type="text" id='usuario' placeholder='Nome do usuario'/>
+                    <input type="text" id='senha' placeholder='Senha'/>
+
+                    <div className="opcoes-login">
+                        <button type='button' style={{backgroundColor: '#4267B2'}}><SiFacebook/></button>
+                        <button type='button' style={{backgroundColor: '#fff', border: '1.5px solid #e6e6e6'}}><FcGoogle/></button>
+                        <button type='button' style={{backgroundColor: '#000'}}><SiApple/></button>
+                    </div>
+
+                    <div className="checkbox">
+                        <input type="checkbox" name="manter-login" id="manter-login"/> <label htmlFor="manter-login">Mater login</label>
+                    </div> 
+
+                    <button type='submit'><FiArrowRight/></button>
+                </form>
+
+                <div className="links">
+                    <a href="/">Não consegue iniciar sessão?</a>
+                    <a href="/">Criar conta</a>
+                </div>
+
+            </div>
+            <div className="container-wallpaper">
+                <img src={wallpaper} alt="wallpaper"/>
+                <button><FaCog/></button>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
